e8d8d983471a54874d70fccb7cf8ff3d5ecd7513
Sam <samhall19@hotmail.co.uk>
2018-01-26 18:50:26 +0000

    pp

---
+++ RadioActive/Assets/Scripts/StationChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StationChanger : MonoBehaviour
+{
+
+    private float currentFreq;
+    public Text currentFreqText;
+	void Start ()
+    {
+        
+	}
+	
+	void Update ()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") != 0.0f) // forward
+        {
+            currentFreq = currentFreq + 0.5f;
+            currentFreqText.text = ("Current Frequency: " + currentFreq);
+            Debug.Log(currentFreqText.text);
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel")) // backwards
+        {
+            minimap.orthographicSize--;
+        }
+    }
+}
